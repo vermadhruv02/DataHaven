dc40cd843da655edb52cb9b18eb667fcd92156da
vermadhruv02 <dhruvveram.dev@gmail.com>
2024-12-30 13:58:20 +0530

    Implement utility functions for error handling, response management, and async operations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import dotenv from 'dotenv';
 import  connectToDatabase from './db/index.js';
+import app from './app.js';
+
 
 dotenv.config();
-connectToDatabase();
+
+connectToDatabase()
+.then(() => {
+    app.on('error', (error) => {
+        console.log("Their is an error",error);
+        throw error;
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is listening on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("catched connect to database error:",error);
+    throw error;
+});
