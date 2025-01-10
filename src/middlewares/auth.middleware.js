8675fb050b6acd6a27de9c1f78ef43e24c68d757
vermadhruv02 <dhruvverma.dev@gmail.com>
2025-01-11 00:34:39 +0530

    Add comment, like,  playlist and tweet models

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/ApiError.js'
-import { User } from '../models/user.models.js'
+import { User } from '../models/user.model.js'
 
 
 export const verifyJWT = asyncHandler(async (req,_ ,next)=>{
---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        comment:{
+            type: mongoose.Types.ObjectId,
+            ref: "Comment"
+        },
+        videl:{
+            type: mongoose.Types.ObjectId,
+            ref: "Video"
+        },
+        likedBy:{
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        },
+        tweet:{
+            type: mongoose.Types.ObjectId,
+            ref: "Tweet"
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Like = mongoose.model("Like",likeSchema);
